e77954d1f2baa420121d581b94f4486cb6e5715b
Tahir Naushasd <t.naushad25@gmail.com>
2017-09-03 21:28:46 +0100

    status code pages

---
+++ Fiver.Mvc.StatusError/Controllers/ErrorsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiver.Mvc.StatusError.Controllers
+{
+    public class ErrorsController : Controller
+    {
+        public IActionResult Index(int statusCode)
+        {
+            var feature = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+
+            ViewBag.StatusCode = statusCode;
+            ViewBag.OriginalPath = feature.OriginalPath;
+            ViewBag.OriginalQueryString = feature.OriginalQueryString;
+
+            return View();
+        }
+    }
+}
---
+++ Fiver.Mvc.StatusError/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fiver.Mvc.StatusError.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public IActionResult Throw404(int id) => StatusCode(404);
+
+        public IActionResult Throw412(int id) => StatusCode(412);
+
+        public IActionResult Throw500(int id) => StatusCode(500);
+    }
+}
